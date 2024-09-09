8fca926695c0488ac39b07936268f6ad008fb71d
Touhidul <toouhidul@gmail.com>
2024-09-09 15:52:30 +0600

    orders model is added

--- orders/models.py
+++ orders/models.py
@@ -12,5 +12,17 @@ class Order(models.Model):
         ('Small' , 'small'),
     )
 
+    order_type = (
+        ('Pending' , 'pending'),
+        ('On delivery' , 'on delivery'),
+        ('delivered' , 'delivered'),
+    )
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    size = models.PositiveSmallIntegerField()
+    size = models.CharField(max_length=20, choices=order_size, default=order_size[0][0])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    order_type = models.CharField(max_length=20, choices=order_type, default=order_type[0][0])
+
+    def __str__(self):
+        return  f'f< Order is {self.size} is created at {self.created_at} by {self.user}'
