57b56c479ff396389c0c67216563bf6d2d658b92
Touhidul <toouhidul@gmail.com>
2024-09-09 15:23:22 +0600

    project is updated

--- authentication/apps.py
+++ authentication/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+'''
+git rm -r --cached venv/
+git rm -r --cached .env
+git rm -r --cached db.sqlite3
+git rm -r --cached static/
+git rm -r --cached media/
+
+'''
